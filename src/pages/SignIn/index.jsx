ca8c0d95e39896eadc4cce3c9daba781e8cf9b36
Ricardo Alencar <ricfilho0007@gmail.com>
2023-10-17 14:41:08 -0300

    feat: install font Montserrat on project (#1)

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,11 +1,25 @@
 import './signin.css'
+import logo from '../../assets/logo.png'
+import fbicon from '../../assets/fbicon.png'
+import googleicon from '../../assets/googleicon.png'
 
 export default function SignIn() {
     return (
         <div className='background'>
             <div className='ctn-login'>
                 <div className='ctn-content-login'>
-                    
+                    <img src={logo} height={150}/>
+
+                    <div className='opc-login'>
+                        <span>entrar com</span>
+                        
+                        <div className='icon-login'>
+                            <img src={fbicon} height={35}/>
+                            <img src={googleicon} height={35}/>
+                        </div>
+
+                    </div>
+
                 </div>
             </div>
         </div>
