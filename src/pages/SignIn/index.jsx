39a2639febb58496edcc7679add381b8dffda9a8
Ricardo Alencar <ricfilho0007@gmail.com>
2023-10-17 14:28:33 -0300

    feat: creating login screen background and login container (#1)

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,7 +1,13 @@
+import './signin.css'
+
 export default function SignIn() {
     return (
-        <>
-            Sign In
-        </>
+        <div className='background'>
+            <div className='ctn-login'>
+                <div className='ctn-content-login'>
+                    
+                </div>
+            </div>
+        </div>
     )
 }
